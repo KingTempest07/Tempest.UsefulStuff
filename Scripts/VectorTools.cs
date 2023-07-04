2acc4e153c9daa644dfafa1b20ae1ebc3378f715
KingTempest07 <70986132+KingTempest07@users.noreply.github.com>
2023-07-03 23:49:22 -0500

    Added DegreesToVector function

---
+++ Scripts/VectorTools.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using UnityEngine;
+
+namespace Tempest.UsefulStuff {
+    public static class VectorTools {
+        public static Vector2 DegreesToVector(this float input) {
+            return new((float) Math.Cos(input * Math.PI / 180), (float) Math.Sin(input * Math.PI / 180));
+        }
+    }
+}
